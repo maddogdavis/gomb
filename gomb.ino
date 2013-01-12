c23d6b132e109f8f5d3d30f109b15685b860518a
maddogdavis <mag@netstorm.net.au>
2013-01-12 14:25:55 +1000

    first servo reporting in

--- gomb.ino
+++ gomb.ino
@@ -1,11 +1,48 @@
+#include <Servo.h>
 #include "Timer.h"
 
+Servo sl;
+//Servo sr;
+//Servo sc;
 Timer t;
 
+void timer() {
+    digitalWrite(13, digitalRead(13) ^ 1);
+}
+
+void loop() {
+    t.update();
+}
+
+// servo
+
+void angle(Servo *s, int a) {
+    log("angle", a);
+    s->write(a);
+}
+
+// init
+
 void setup() {
+    innit();
+    report();
+}
+
+void innit() {
     init_serial();
     init_pins();
     init_timer();
+    init_servos();
+}
+
+void report() {
+    angle(&sl, 30);
+    delay(500);
+    angle(&sl, 120);
+    delay(500);
+    angle(&sl, 30);
+    delay(500);
+    angle(&sl, 120);
 }
 
 void init_serial() {
@@ -14,18 +51,22 @@ void init_serial() {
 
 void init_pins() {
     pinMode(13, OUTPUT);
-//   pinMode(12, OUTPUT);
 }
 
 void init_timer() {
     t.every(2000, timer);
 }
 
-
-void timer() {
-    digitalWrite(13, digitalRead(13) ^ 1);
+void init_servos() {
+    sl.attach(9);
+//    sr.attach(17);
+//    sc.attach(18);
 }
 
-void loop() {
-    t.update();
+// util
+
+void log(char *m, int v) {
+    Serial.print(m);
+    Serial.print(": ");
+    Serial.println(v, DEC);
 }
